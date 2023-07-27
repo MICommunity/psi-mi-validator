c66e524d8989dc39c82e90d38cd92ada2fbb8419
jmedinaebi <jmedina@ebi.ac.uk>
2023-07-27 09:49:01 +0100

    Update validate scripts to use slurm

--- psimi-schema-validator-cli/validate-xml.sh
+++ psimi-schema-validator-cli/validate-xml.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#SBATCH --time=06-00:00:00   # walltime
+#SBATCH --ntasks=1   # number of tasks
+#SBATCH --cpus-per-task=5   # number of CPUs Per Task i.e if your code is multi-threaded
+#SBATCH -p production   # partition(s)
+#SBATCH --mem=2G   # memory per node
+#SBATCH -J "XML_VALIDATOR"   # job name
+#SBATCH -o "/nfs/production/hhe/intact/data/psi-mi-validator-logs/validate-xml-%j.out"   # job output file
+#SBATCH --mail-user=intact-dev@ebi.ac.uk   # email address
+#SBATCH --mail-type=ALL
+
 FILE=$1
 
 mvn -U clean install -Pvalidate -Dfile="$FILE" -Dscope=syntax -Dlevel=WARN -Dmaven.test.skip
